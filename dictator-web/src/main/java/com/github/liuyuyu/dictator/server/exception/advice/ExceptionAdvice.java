ca663d24c35b4ee3bf179422614364edca330a21
onlyeat3 <onlyeat3@gmail.com>
2018-01-21 22:40:55 +0800

    :sparkles: 客户端集成

--- dictator-web/src/main/java/com/github/liuyuyu/dictator/server/exception/advice/ExceptionAdvice.java
+++ dictator-web/src/main/java/com/github/liuyuyu/dictator/server/exception/advice/ExceptionAdvice.java
@@ -3,7 +3,6 @@ package com.github.liuyuyu.dictator.server.exception.advice;
 import com.github.liuyuyu.dictator.common.ErrorCodeEnum;
 import com.github.liuyuyu.dictator.common.model.response.DataWrapper;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -23,10 +22,11 @@ public class ExceptionAdvice {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public DataWrapper handleMethodArgumentNotValidException(MethodArgumentNotValidException ex){
         DataWrapper dataWrapper = DataWrapper.of(ErrorCodeEnum.INVALID_PARAMETER);
-        List<String> errorMessageList = ex.getBindingResult().getFieldErrors().stream()
+        StringBuilder invalidParameterMsg = new StringBuilder();
+        ex.getBindingResult().getFieldErrors().stream()
                 .map(e-> String.format("参数%s,%s",e.getField(), e.getDefaultMessage()))
-                .collect(Collectors.toList());
-        dataWrapper.setData(errorMessageList);
+                .forEach(s-> invalidParameterMsg.append(s).append(","));
+        dataWrapper.setMsg(invalidParameterMsg.toString());
         log.warn("ex",ex);
         return dataWrapper;
     }
