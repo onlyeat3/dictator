d837fc4f0d1a474edb07af1e0849c580cc94184a
onlyeat3 <onlyeat3@gmail.com>
2018-01-30 18:02:36 +0800

    :fire: rm 无效配置和引用

--- dictator-spring-boot-starter-demo/src/test/java/com/github/liuyuyu/dictator/springboot/config/BeanConfig.java
+++ dictator-spring-boot-starter-demo/src/test/java/com/github/liuyuyu/dictator/springboot/config/BeanConfig.java
@@ -1,8 +1,5 @@
 package com.github.liuyuyu.dictator.springboot.config;
 
-import com.github.liuyuyu.dictator.spring.DictatorAutoRefresher;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
