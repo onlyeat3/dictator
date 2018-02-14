f57917e01f063d1438fd4003ef45b305c786a94f
liuyuyu <liuyuyu@kongzhong.com>
2018-02-14 11:36:44 +0800

    :heavy_minus_sign: Spring security 集成

--- dictator-web/src/main/java/com/github/liuyuyu/dictator/server/controller/UserController.java
+++ dictator-web/src/main/java/com/github/liuyuyu/dictator/server/controller/UserController.java
@@ -3,15 +3,11 @@ package com.github.liuyuyu.dictator.server.controller;
 import com.github.liuyuyu.dictator.common.model.response.DataWrapper;
 import com.github.liuyuyu.dictator.server.common.model.request.LoginRequest;
 import com.github.liuyuyu.dictator.server.common.model.response.UserInfoResponse;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.web.WebAttributes;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.UUID;
@@ -27,24 +23,6 @@ public class UserController {
     public DataWrapper login(@RequestBody @Valid LoginRequest loginRequest) {
         String token = UUID.randomUUID().toString().replaceAll("-", "");
         return DataWrapper.from(token);
-    }
-
-    @RequestMapping("/loginFail")
-    public DataWrapper loginFail(HttpServletRequest request) {
-        Throwable e = (Throwable) request.getAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
-        DataWrapper dataWrapper = DataWrapper.of();
-        String msg = "未知原因导致登录失败";
-        String code = "UNKNOWN_FAIL";
-        if(e != null){
-            if(e instanceof UsernameNotFoundException || e instanceof BadCredentialsException){
-                code = "INCORRECT_USERNAME_OR_PASSWORD";
-                msg = "用户名或密码错误";
-            }
-        }
-        dataWrapper.setMsg(msg);
-        dataWrapper.setCode(code);
-        dataWrapper.setSuccess(Boolean.FALSE);
-        return dataWrapper;
     }
 
     @RequestMapping("/info")
