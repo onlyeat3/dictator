222a2bdd570b5f5b7e3ae600ece7e2067e400cbf
liuyuyu <liuyuyu2333@gmail.com>
2018-02-18 13:24:24 +0800

    :fire: 因为版本依赖的elementui版本过低,去掉vat

--- dictator-web/frontend/README.md
+++
@@ -1,1 +0,0 @@
-from https://github.com/PanJiaChen/vueAdmin-template
--- dictator-web/frontend/src/api/config.js
+++
@@ -1,8 +0,0 @@
-import fetch from '@/utils/fetch'
-
-export function list(params){
-    return fetch({
-        url: "/config/list",
-        params
-    })
-}
