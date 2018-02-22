bd7800cfb5af74873511d602725624ea055fabd1
liuyuyu <liuyuyu@kongzhong.com>
2018-02-22 12:55:50 +0800

    :bug: 空id list缺少查询检查

--- dictator-web/src/main/java/com/github/liuyuyu/dictator/server/mapper/DictatorConfigGroupMapper.java
+++ dictator-web/src/main/java/com/github/liuyuyu/dictator/server/mapper/DictatorConfigGroupMapper.java
@@ -1,6 +1,7 @@
 package com.github.liuyuyu.dictator.server.mapper;
 
 import com.github.liuyuyu.dictator.server.model.entity.DictatorConfigGroup;
+import com.google.common.collect.Maps;
 import lombok.NonNull;
 import org.apache.ibatis.annotations.Mapper;
 import tk.mybatis.mapper.weekend.Weekend;
@@ -12,6 +13,9 @@ import java.util.stream.Collectors;
 @Mapper
 public interface DictatorConfigGroupMapper extends SimpleMapper<DictatorConfigGroup> {
     default Map<Long,String> findConfigNameMapByGroupIdList(@NonNull List<Long> configIdList){
+        if(configIdList.isEmpty()){
+            return Maps.newHashMap();
+        }
         Weekend<DictatorConfigGroup> weekend = Weekend.of(DictatorConfigGroup.class);
         weekend.weekendCriteria()
                 .andIn(DictatorConfigGroup::getId,configIdList);
--- dictator-web/src/main/java/com/github/liuyuyu/dictator/server/mapper/DictatorConfigProfileMapper.java
+++ dictator-web/src/main/java/com/github/liuyuyu/dictator/server/mapper/DictatorConfigProfileMapper.java
@@ -1,6 +1,7 @@
 package com.github.liuyuyu.dictator.server.mapper;
 
 import com.github.liuyuyu.dictator.server.model.entity.DictatorConfigProfile;
+import com.google.common.collect.Maps;
 import lombok.NonNull;
 import org.apache.ibatis.annotations.Mapper;
 import tk.mybatis.mapper.weekend.Weekend;
@@ -13,6 +14,9 @@ import java.util.stream.Collectors;
 @Mapper
 public interface DictatorConfigProfileMapper extends SimpleMapper<DictatorConfigProfile> {
     default Map<Long, String> findProfileNameByIdList(@NonNull List<Long> profileIdList) {
+        if(profileIdList.isEmpty()){
+            return Maps.newHashMap();
+        }
         Weekend<DictatorConfigProfile> weekend = Weekend.of(DictatorConfigProfile.class);
         weekend.weekendCriteria()
                 .andIn(DictatorConfigProfile::getId,profileIdList);
