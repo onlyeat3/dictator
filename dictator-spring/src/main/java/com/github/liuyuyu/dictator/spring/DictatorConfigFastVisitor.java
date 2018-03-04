e5fe471469ec5e5648e930b902be88acfdddb5d3
liuyuyu <liuyuyu2333@gmail.com>
2018-03-04 12:17:38 +0800

    :sparkles: Java API获取配置

---
+++ dictator-spring/src/main/java/com/github/liuyuyu/dictator/spring/DictatorConfigFastVisitor.java
@@ -0,0 +1,15 @@
+package com.github.liuyuyu.dictator.spring;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+/*
+ * @author liuyuyu
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class DictatorConfigFastVisitor {
+    public static String get(@NonNull String name){
+        return DictatorPropertyManager.getProperty(name);
+    }
+}
