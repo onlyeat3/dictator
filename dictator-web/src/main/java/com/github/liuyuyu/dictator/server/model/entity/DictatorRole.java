db1914730faea742331563337d5ad4aef4334878
onlyeat3 <onlyeat3@gmail.com>
2018-03-04 17:17:43 +0800

    :art: 组织import

--- dictator-web/src/main/java/com/github/liuyuyu/dictator/server/model/entity/DictatorRole.java
+++ dictator-web/src/main/java/com/github/liuyuyu/dictator/server/model/entity/DictatorRole.java
@@ -1,9 +1,10 @@
 package com.github.liuyuyu.dictator.server.model.entity;
 
-import java.util.Date;
-import javax.persistence.*;
+import lombok.Data;
 
-import lombok.*;
+import javax.persistence.Column;
+import javax.persistence.Table;
+import java.util.Date;
 
 @Data
 @Table(name = "dictator_role")
