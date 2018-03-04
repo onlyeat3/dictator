c3b841a32fba2670d9124ea8177f43be37717d9a
onlyeat3 <onlyeat3@gmail.com>
2018-03-04 17:14:38 +0800

    :bug: 补充测试，修复第一次获取配置没有时间戳导致的异常

---
+++ dictator-web/src/test/java/com/github/liuyuyu/dictator/server/core/service/zookeeper/ZookeeperReadServiceTest.java
@@ -0,0 +1,31 @@
+package com.github.liuyuyu.dictator.server.core.service.zookeeper;
+
+import com.github.liuyuyu.dictator.server.core.service.AbstractConfigReadServiceTest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/*
+ * @author liuyuyu
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZookeeperReadServiceTest extends AbstractConfigReadServiceTest {
+
+    @Autowired private ZookeeperConfigService zookeeperConfigService;
+
+    @Override
+    public void setConfigReadService() {
+        super.configReadService = this.zookeeperConfigService;
+    }
+
+    @Test
+    public void findAll() {
+        super.findAll();
+    }
+}
