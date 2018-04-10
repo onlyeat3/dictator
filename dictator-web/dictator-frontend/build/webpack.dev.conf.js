50beb6fba79e3df772781ac2342236a6d374fdca
liuyuyu <liuyuyu2333@gmail.com>
2018-04-10 22:15:53 +0800

    :bug: dev环境html文件没有自动更新引用

--- dictator-web/dictator-frontend/build/webpack.dev.conf.js
+++ dictator-web/dictator-frontend/build/webpack.dev.conf.js
@@ -8,6 +8,8 @@ const baseWebpackConfig = require('./webpack.base.conf')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 const portfinder = require('portfinder')
+const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
+
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
@@ -52,12 +54,14 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     // https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
       filename: config.build.index,
-      template: config.build.index,
+      template: 'index.html',
       inject: true,
+      alwaysWriteToDisk: true,
       favicon: resolve('favicon.ico'),
       title: 'vue-element-admin',
       path: config.dev.assetsPublicPath + config.dev.assetsSubDirectory
     }),
+    new HtmlWebpackHarddiskPlugin()
   ]
 })
 
--- dictator-web/dictator-frontend/build/webpack.prod.conf.js
+++ dictator-web/dictator-frontend/build/webpack.prod.conf.js
@@ -65,7 +65,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     // see https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
       filename: config.build.index,
-      template: config.build.index,
+      template: 'index.html',
       inject: true,
       favicon: resolve('favicon.ico'),
       title: 'vue-element-admin',
