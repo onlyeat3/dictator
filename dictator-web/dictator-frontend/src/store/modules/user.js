f0b0b5094d295c5eaf22ed902eb5268601cc05de
liuyuyu <liuyuyu@kongzhong.com>
2018-04-25 17:38:32 +0800

    :art: rm console.log

--- dictator-web/dictator-frontend/src/store/modules/user.js
+++ dictator-web/dictator-frontend/src/store/modules/user.js
@@ -41,7 +41,6 @@ const user = {
       state.avatar = 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif';
     },
     SET_RESOURCE_LIST: (state, resourceList) => {
-      console.log(state.resourceList);
       state.resourceList = resourceList
     }
   },
