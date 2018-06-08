a472f8d707907ab690e884e3d75cb2f7676dd805
liuyuyu <liuyuyu2333@gmail.com>
2018-06-08 16:20:58 +0800

    :sparkles: APP表结构

--- sql/v0.0.3.sql
+++ sql/v0.0.3.sql
@@ -13,3 +13,22 @@ CREATE TABLE IF NOT EXISTS `dictator_role_profile` (
   KEY `idx_operator_id` (`operator_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='角色-环境关联';
 ALTER TABLE `dictator_user` ADD UNIQUE INDEX `uq_user_name`(`user_name`) USING BTREE;
+
+create table dictator_app
+(
+    `id` bigint NOT NULL primary key AUTO_INCREMENT COMMENT '自增主键',
+    app_name varchar(50) not null comment 'app名称',
+    app_id varchar(50) not null comment 'APP ID',
+    owner_email varchar(50) not null comment '所有者邮箱',
+    `created_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `updated_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+    `operator_id` bigint(20) NOT NULL COMMENT '操作者ID',
+    `operator_ip` varchar(50) NOT NULL COMMENT '操作者IP',
+    UNIQUE KEY `uq_app_id` (`app_id`),
+    KEY `idx_created_time` (`created_time`),
+    KEY `idx_updated_time` (`updated_time`),
+    KEY `idx_operator_id` (`operator_id`)
+)engine=InnoDB comment 'APP';
+
+ALTER TABLE `dictator`.`dictator_config` MODIFY COLUMN `app_id` bigint NOT NULL COMMENT '应用（服务）ID' AFTER `id`;
+ALTER TABLE `dictator`.`dictator_config_history` MODIFY COLUMN `app_id` bigint NOT NULL COMMENT '应用（服务）ID' AFTER `id`;
