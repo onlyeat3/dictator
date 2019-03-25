6b23852a9c599ce449f67909bb4ab1368cfadef0
onlyeat3 <onlyeat3@gmail.com>
2019-03-26 00:26:18 +0800

    内嵌了一个MySQL做单元测试

--- dictator-web/src/test/java/com/github/liuyuyu/dictator/server/constant/TestConstant.java
+++ dictator-web/src/test/java/com/github/liuyuyu/dictator/server/constant/TestConstant.java
@@ -10,4 +10,5 @@ import lombok.NoArgsConstructor;
 public class TestConstant {
     public static final String IP = "0.0.0.0";
     public static final Long defaultUserId = 0L;
+    public static final int embedMySQLPort = 3366;
 }
