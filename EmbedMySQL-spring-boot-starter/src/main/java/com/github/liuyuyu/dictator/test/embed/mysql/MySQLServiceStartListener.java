a5685637836458cd0827c1c7ac9888894e22de35
liuyuyu <liuyuyu2333@gmail.com>
2019-03-28 14:51:58 +0800

    flyway for web & EembedMySQL for test

--- EmbedMySQL-spring-boot-starter/src/main/java/com/github/liuyuyu/dictator/test/embed/mysql/MySQLServiceStartListener.java
+++ EmbedMySQL-spring-boot-starter/src/main/java/com/github/liuyuyu/dictator/test/embed/mysql/MySQLServiceStartListener.java
@@ -1,13 +1,21 @@
 package com.github.liuyuyu.dictator.test.embed.mysql;
 
 import com.wix.mysql.EmbeddedMysql;
+import com.wix.mysql.Sources;
+import com.wix.mysql.SqlScriptSource;
 import com.wix.mysql.config.MysqldConfig;
 import com.wix.mysql.config.SchemaConfig;
+import org.apache.commons.io.FileUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringApplicationRunListener;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.io.Resource;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static com.wix.mysql.EmbeddedMysql.anEmbeddedMysql;
@@ -43,6 +51,27 @@ public class MySQLServiceStartListener implements SpringApplicationRunListener {
         String databaseName = environment.getProperty("spring.datasource.databaseName","test");
         String user = environment.getProperty("spring.datasource.username","");
         String password = environment.getProperty("spring.datasource.password","");
+        List<String> dataFileLocations = Arrays.asList(environment.getProperty("spring.datasource.script-locations", String[].class));
+        String[] moreFileLocations = environment.getProperty("spring.datasource.scriptLocations", String[].class);
+        if (moreFileLocations != null) {
+            dataFileLocations.addAll(Arrays.asList(moreFileLocations));
+        }
+
+        List<Resource> resourcesList = new ArrayList<>();
+        if (dataFileLocations != null) {
+            for (String dataFileLocation : dataFileLocations) {
+                try {
+                    Resource[] resources = configurableApplicationContext.getResources(dataFileLocation);
+                    resourcesList.addAll(Arrays.asList(resources));
+                } catch (IOException e) {
+                    //ignore
+                }
+            }
+        }
+
+//        Path path = Paths.get(resourcesList.get(0).getURI());
+//        String sql = new String(Files.readAllBytes(path));
+
         MysqldConfig config = aMysqldConfig(v5_6_23)
                 .withCharset(UTF8)
                 .withPort(port)
@@ -52,16 +81,16 @@ public class MySQLServiceStartListener implements SpringApplicationRunListener {
                 .withServerVariable("max_connect_errors", 666)
                 .build();
 
-
-        SchemaConfig cfg = SchemaConfig.aSchemaConfig(databaseName)
-                .build();
         EmbeddedMysql mysqlServer = anEmbeddedMysql(config).start();
-        if (!MySQLProperties.DEFAULT_TEST_DB.equals(databaseName)) {
-//            mysqlServer.dropSchema(cfg);
-//            mysqlServer.addSchema(cfg);
+        if (!resourcesList.isEmpty()) {
+            for (Resource resource : resourcesList) {
+                try {
+                    mysqlServer.executeScripts(databaseName,Sources.fromFile(resource.getFile()));
+                } catch (IOException e) {
+                    throw new RuntimeException(String.format("data file:%s load fail",resource.getFilename()),e);
+                }
+            }
         }
-
-
     }
 
     @Override
