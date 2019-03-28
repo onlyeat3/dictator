7c9486eb91040492a14c8d6cefeafa3d83bf1e91
onlyeat3 <onlyeat3@gmail.com>
2019-03-28 14:51:58 +0800

    flyway for web & EembedMySQL for test

--- EmbedMySQL-spring-boot-starter/src/main/java/com/github/liuyuyu/dictator/test/embed/mysql/MySQLProperties.java
+++ EmbedMySQL-spring-boot-starter/src/main/java/com/github/liuyuyu/dictator/test/embed/mysql/MySQLProperties.java
@@ -5,6 +5,13 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Data
+@ConfigurationProperties(prefix = "spring.datasource")
 public class MySQLProperties extends DataSourceProperties {
     public static final String DEFAULT_TEST_DB = "test";
+
+    private String[] scriptLocations;
+
+    private Integer port;
+
+    private String databaseName;
 }
