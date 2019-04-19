8971c1051bf3afc2d23fc44b163b188296a89f4a
liuyuyu <liuyuyu2333@gmail.com>
2019-04-19 15:52:39 +0800

    简化embedded-mysql逻辑，略微干净的实现方式

--- dictator-web/src/test/java/com/github/liuyuyu/dictator/server/web/service/DictatorAppServiceTest.java
+++ dictator-web/src/test/java/com/github/liuyuyu/dictator/server/web/service/DictatorAppServiceTest.java
@@ -7,6 +7,7 @@ import com.github.liuyuyu.dictator.server.web.model.param.AppSaveOrUpdateParam;
 import com.github.liuyuyu.dictator.server.web.model.param.DeleteParam;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 
